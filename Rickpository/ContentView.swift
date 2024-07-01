2182b84a81bb0c10a90624f57880c925a62ec4b5
laetuz <laetuzg@gmail.com>
2024-07-01 21:39:59 +0700

    Initial Commit

---
+++ Rickpository/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rickpository
+//
+//  Created by Ryo Martin on 01/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rickpository/RickpositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickpositoryApp.swift
+//  Rickpository
+//
+//  Created by Ryo Martin on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RickpositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
